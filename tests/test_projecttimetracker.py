d19685cb3c1285eaafb9f0eaa543674321a8e35d
reustonium <andrew.ruestow@gmail.com>
2015-09-12 22:31:34 -0400

    used clirunner to add a real test

--- tests/test_projecttimetracker.py
+++ tests/test_projecttimetracker.py
@@ -9,6 +9,7 @@ Tests for `projecttimetracker` module.
 """
 
 import unittest
+from click.testing import CliRunner
 
 from projecttimetracker import projecttimetracker
 
@@ -18,8 +19,11 @@ class TestProjecttimetracker(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_something(self):
-        self.assertEqual(1, 1)
+    def test_hw(self):
+        runner = CliRunner()
+        result = runner.invoke(projecttimetracker.cli)
+        assert result.exit_code == 0
+        assert result.output == 'hiya\n'
 
     def tearDown(self):
         pass
